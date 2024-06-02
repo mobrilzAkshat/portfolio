45465f0b39d37dd06ae0bcad31379802f8cc9a39
akshatchawla1997 <akshatchawla1997@gmail.com>
2024-06-02 23:56:53 +0530

    createblog added

---
+++ src/pages/blogs/CreateBlog.tsx
@@ -0,0 +1,16 @@
+function CreateBlog() {
+  return (
+    <>
+      <div className="">
+        <form action="">
+          <div>
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default CreateBlog;
