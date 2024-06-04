4011c7e1b46ae700164f4f178996989991396b4b
akshatchawla1997 <akshatchawla1997@gmail.com>
2024-06-04 19:03:42 +0530

    logo changed

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { HiMenu, HiX } from "react-icons/hi";
 import NavigationLinks from "./NavigationLinks";
+import Akshat_logo from '../../assets/CompanyLogo/Akshat_logo.png'
 
 export function Header() {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -33,9 +34,9 @@ export function Header() {
       </div>
       <div className={`${isMobileMenuOpen ? "block" : "hidden lg:block"} lg:flex items-center justify-between`}>
         <div className="lg:flex items-center hidden">
-          <div className="logo w-20 h-12 bg-gray-700 flex justify-center">
-          {/* <img src={logo} alt="" className="rounded-full w-20 bg-transparent" /> */}
-            <h2 className="p-2 ">Akshat</h2>
+          <div className="logo w-24 h-24 h-12 flex justify-center">
+          <img src={Akshat_logo} alt="Profile logo" className="rounded-full w-20 bg-transparent" />
+            
           </div>
         </div>
         <NavigationLinks closeMobileMenu={closeMobileMenu} isMobileMenuOpen={isMobileMenuOpen} isMounted={isMounted} />
