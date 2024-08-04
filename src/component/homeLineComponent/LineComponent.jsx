eab1c3e483eb1b51ceaf639d11382546d0e2fd09
akshatchawla1997 <akshatchawla1997@gmail.com>
2024-08-05 01:24:35 +0530

    Line Component added

---
+++ src/component/homeLineComponent/LineComponent.jsx
@@ -0,0 +1,432 @@
+import Image from "../../assets/images";
+import React from "react";
+
+const LineSection = () => {
+  return (
+    <div className="w-full flex gap-24">
+      <div className="flex w-full gap-24">
+        <div className="w-3/5">
+          <div className="w-full flex justify-center items-center">
+            <div className="w-full ">
+              <h4 className="font-bold text-4xl pb-16 flex justify-center">
+                APPS
+              </h4>
+              <div className="flex justify-center items-center">
+                <div className="vertical_name font-normal text-2xl text-[#2E6AD6]">
+                  BACKEND
+                </div>
+                <div className="bg-[#DFEFFC] w-full rounded-2xl py-4">
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src=""
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Laravel
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 w-full items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/032.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Adonis JS
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 w-full items-center justify-start py-2 pl-12">
+                    <Image
+                      src="/images/033.png"
+                      width={42}
+                      height={43}
+                    ></Image>
+
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Express JS
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="w-full">
+              <div className="flex flex-col gap-1 items-center ">
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+              </div>
+              <div className="flex justify-center items-center">
+                <div className="vertical_name font-normal text-2xl text-[#2E6AD6]">
+                  FRONTEND
+                </div>
+                <div className="bg-[#DFEFFC] w-full rounded-2xl py-4">
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Vue JS
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/007adc343e0fda03e4dc70faa1212266.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Nuxt JS
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/b51c30e9f2c018b1d44a39994e46022e.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      React JS
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="w-full mb-10">
+              <div className="flex flex-col gap-1 items-center ">
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+              </div>
+              <div className="flex justify-start items-center">
+                <div className="vertical_name font-normal text-2xl text-[#2E6AD6]">
+                  DATABASE
+                </div>
+                <div className="bg-[#DFEFFC] w-full rounded-2xl py-4">
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <Image
+                      src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                      width={42}
+                      height={43}
+                    ></Image>
+
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      MySQL
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <Image
+                      src="/images/007adc343e0fda03e4dc70faa1212266.png"
+                      width={42}
+                      height={43}
+                    ></Image>
+
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Rethink DB
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <Image
+                      src="/images/b51c30e9f2c018b1d44a39994e46022e.png"
+                      width={42}
+                      height={43}
+                    ></Image>
+
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Mongo DB
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <Image
+                      src="/images/b51c30e9f2c018b1d44a39994e46022e.png"
+                      width={42}
+                      height={43}
+                    ></Image>
+
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Fauna DB
+                    </h4>
+                  </div>
+
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                </div>
+              </div>
+              <div className=" flex w-full">
+                <div className="dotted-border4  ml-52 w-full h-56 "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2 ">
+          <h4 className="font-bold text-4xl pb-16 flex justify-center">API</h4>
+          <div className="bg-[#DFEFFC] w-full rounded-2xl py-4">
+            <div className="flex gap-2 items-center justify-start py-2 pl-12">
+              <Image
+                src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                width={42}
+                height={43}
+              ></Image>
+
+              <h4 className="text-[#2E6AD6] font-normal text-4xl">Laravel</h4>
+            </div>
+            <div className="pl-12">
+              <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+            </div>
+            <div className="flex gap-2 items-center justify-start py-2 pl-12">
+              <Image
+                src="/images/007adc343e0fda03e4dc70faa1212266.png"
+                width={42}
+                height={43}
+              ></Image>
+
+              <h4 className="text-[#2E6AD6] font-normal text-4xl">Adonis JS</h4>
+            </div>
+            <div className="pl-12">
+              <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+            </div>
+            <div className="flex gap-2 items-center justify-start py-2 pl-12">
+              <Image
+                src="/images/b51c30e9f2c018b1d44a39994e46022e.png"
+                width={42}
+                height={43}
+              ></Image>
+
+              <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                Express JS
+              </h4>
+            </div>
+            <div className="pl-12">
+              <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+            </div>
+            <div className="flex gap-2 items-center justify-start py-2 pl-12">
+              <Image
+                src="/images/b51c30e9f2c018b1d44a39994e46022e.png"
+                width={42}
+                height={43}
+              ></Image>
+
+              <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                Express JS
+              </h4>
+            </div>
+            <div className="pl-12">
+              <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+            </div>
+            <div className="flex gap-2 items-center justify-start py-2 pl-12">
+              <Image
+                src="/images/b51c30e9f2c018b1d44a39994e46022e.png"
+                width={42}
+                height={43}
+              ></Image>
+
+              <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                Express JS
+              </h4>
+            </div>
+            <div className="pl-12">
+              <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+            </div>
+            <div className="flex gap-2 items-center justify-start py-2 pl-12">
+              <Image
+                src="/images/b51c30e9f2c018b1d44a39994e46022e.png"
+                width={42}
+                height={43}
+              ></Image>
+
+              <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                Express JS
+              </h4>
+            </div>
+            <div className="pl-12">
+              <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="flex flex-col items-center justify-center w-full">
+          <div className="flex gap-8 items-start pt-10 justify-center w-3/4 relative z-20">
+            <div className="w-full flex flex-col item justify-center ">
+              <h4 className="font-bold text-4xl pb-16 flex justify-center">
+                iOS
+              </h4>
+              <div className="flex justify-center items-center">
+                <div className="bg-[#DFEFFC] w-full rounded-2xl py-4">
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Swift
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full flex flex-col item justify-center ">
+              <h4 className="font-bold text-4xl pb-16 flex justify-center">
+                ANDROID
+              </h4>
+              <div className="flex justify-center items-center">
+                <div className="bg-[#DFEFFC] w-full rounded-2xl py-4">
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Swift
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px "></div>
+                  </div>
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Swift
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center w-full rel">
+            <div className="w-2/3  flex flex-col justify-center ">
+              <div className=" flex justify-center">
+                <div className="dotted-border2 w-2/3 h-48 relative z-10 top-[-50px]"></div>
+              </div>
+              <div className="flex flex-col gap-1 items-center mt-[-49px] ">
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+                <div className="dot-line"></div>
+              </div>
+              <h4 className="font-bold text-4xl pb-6 flex justify-center">
+                Hybrid Apps Tech Stack
+              </h4>
+              <div className="flex justify-center items-center">
+                <div className="bg-[#DFEFFC] w-3/5 rounded-2xl py-4">
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Swift
+                    </h4>
+                  </div>
+                  <div className="pl-12">
+                    <div className="bg-[#7C9BD3] w-4/5 h-px ">
+                      <div className=" flex">
+                        <div className="dotted-border3 w-3/4 h-96 mt-24"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex gap-2 items-center justify-start py-2 pl-12">
+                    <div className="">
+                      <Image
+                        src="/images/fbeb1e18ca8b257d0ed5516e767c9e99.png"
+                        width={42}
+                        height={43}
+                      ></Image>
+                    </div>
+                    <h4 className="text-[#2E6AD6] font-normal text-4xl">
+                      Swift
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LineSection;
